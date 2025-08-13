9630ce466a52d9637e18b35f93f7b92c92c49fa9
Ernesto Pedrosa Alonso <e.pedrosaalonso@celonis.com>
2025-08-13 16:31:31 +0200

    feat: angular root improvemetns

--- .config/nvim/lua/plugins/angular.lua
+++ .config/nvim/lua/plugins/angular.lua
@@ -1,3 +1,4 @@
+local calculate_root_dir = nil
 return {
   {
     "neovim/nvim-lspconfig",
@@ -5,11 +6,18 @@ return {
       servers = {
         angularls = {
           root_dir = function(fname)
-            local nx_root = require("lspconfig.util").root_pattern("nx.json")(fname)
-            if nx_root then
-              return nx_root
+            if calculate_root_dir == nil then
+              -- Calculate the root directory only once
+              local nx_root = require("lspconfig.util").root_pattern("nx.json")(fname)
+              if nx_root then
+                calculate_root_dir = nx_root
+                return calculate_root_dir
+              end
+
+              calculate_root_dir = require("lspconfig.util").root_pattern("angular.json", "project.json")(fname)
+              return calculate_root_dir
             end
-            return require("lspconfig.util").root_pattern("angular.json", "project.json")(fname)
+            return calculate_root_dir
           end,
         },
       },
