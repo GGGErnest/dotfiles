9b737413063b8e2ffdc4652f68af55d347fa05e3
Ernesto Pedrosa Alonso <e.pedrosaalonso@celonis.com>
2026-01-13 17:00:17 +0100

    feat: remove unnecessary sections in lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_z, require("opencode").statusline)
+    table.remove(opts.sections.lualine_b)
+    table.remove(opts.sections.lualine_c)
+
+    -- Add env-switcher component to show current .env configuration
+    local env_switcher = require("utils.env_switcher")
+    table.insert(opts.sections.lualine_x, 1, {
+      env_switcher.lualine_component,
+      cond = env_switcher.lualine_cond,
+    })
+  end,
+}
